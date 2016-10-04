ea07443ce77bec5e8679854df1702885ae01ba6b
Alan Denison <alan79td@gmail.com>
2016-10-04 13:24:52 -0700

    create db context

---
+++ CustomerService/CustomerService/Data/CustomerServiceContext.cs
@@ -0,0 +1,22 @@
+﻿using CustomerService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerService.Data
+{
+    public class CustomerServiceContext : DbContext
+    {
+        public CustomerServiceContext(DbContextOptions<CustomerServiceContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Line> Lines { get; set; }
+        public DbSet<CustomerLine> CustomerLines { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Line>().ToTable("Line");
+            modelBuilder.Entity<CustomerLine>().ToTable("CustomerLine");
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+        }
+    }
+}
